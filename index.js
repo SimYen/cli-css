2c783c28af1af1511c0e0e9e8047418f4d5e33de
SYNG <simyenng@gmail.com>
2019-10-03 18:33:43 +0800

    Completed hex to HSL

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ console.log("Color Converter Instructions:")
 console.log("To convert from hex color to rgb color, enter \"hex \\hexColor\".")
 console.log("To convert from rgb color to hex color, enter \"rgb r g b.\"")
 
-const hex = require('./hexToRgb')
-const rgb = require('./rgbToHex')
+const hexRgb = require('./hexToRgb')
+const rgbHex = require('./rgbToHex')
+const hexHSL = require('./toHsl')
 
-if ( process.argv[2] === "hex" ) {
-    console.log ( "rgb \( " + hex.hexToRgb(process.argv[3]).r + ", " + hex.hexToRgb(process.argv[3]).g + ", "  + hex.hexToRgb(process.argv[3]).b + " \)")
-} else if ( process.argv[2] === "rgb" ) {
-    console.log ( rgb.rgbToHex( parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5]) ) )
+switch ( process.argv[2] ) {
+    case 'hex':
+    console.log ( "rgb \( " + hexRgb.hexToRgb(process.argv[3]).r + ", " + hexRgb.hexToRgb(process.argv[3]).g + ", "  + hexRgb.hexToRgb(process.argv[3]).b + " \)");
+    console.log ( hexHSL.hexToHsl(process.argv[3]));
+    break;
+    case 'rgb':
+    console.log ( rgbHex.rgbToHex( parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5]) ) );
+    break;
 }
